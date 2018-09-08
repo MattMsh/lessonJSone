a2fd6a2119208566264a4da3b93cea1589e356c9
Olexandr Galutskiy <kniazgal@gmail.com>
2018-09-08 13:05:44 +0300

    available

--- assets/app.js
+++ assets/app.js
@@ -14,23 +14,55 @@ var buildingInfoHtml = '<div class="flat-info show">'+
 		'</div>'+
 	'</div>';
 
-	// axios.get('/assets/flats.json').then(function(response){
+	var buindingInfo = {
+		b1: {
+			price: 0,
+			available: 0
+		},
+		b2: {
+			price: 0,
+			available: 0	
+		}
+	}
+
+	axios.get('/assets/flats.json').then(function(response){
 		
-	// });
+		var flats = response.data;
 
 
-	mouseOver = function(building, event){
+		flats.forEach(function(flat){
+			if(flat.building == 1 && flat.available){
+				buindingInfo.b1.available++;
+			}
+			if(flat.building == 2 && flat.available){
+				buindingInfo.b2.available++;
+			}
+
+		});
+
+	}).catch(function(error){
+		console.log(error);
+	})
+
+
+
+	mouseOver = function(building){
 		var buildingInfo = document.getElementById('buildingInfo');
-		buildingInfo.style.left = event.offsetX +'px';
-		buildingInfo.style.top = event.offsetY +'px';
 		buildingInfo.style.display = 'block';
 
+		var available = buindingInfo['b'+building].available;
+		var price = buindingInfo['b'+building].price;
 
 		buildingInfo.innerHTML = buildingInfoHtml
 			.replace('{building}', building)
-			.replace('{available}', 0)
-			.replace('{price}', 0);
+			.replace('{available}', available)
+			.replace('{price}', price);
 	}
+
+
+
+
+
 	mouseOut = function(){
 		var buildingInfo = document.getElementById('buildingInfo');
 		buildingInfo.style.display = 'none';
